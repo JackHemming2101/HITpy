91d360206aa9eec3d2d2bb6a228a2520f39c2626
GlavPoci <glavpoci@gmail.com>
2017-09-22 13:24:06 +0300

    Version 0.2 Перевёл Math к себе. Тестовая Генерация резисторов. Групировка резисторов вход-выход.

--- index.py
+++ index.py
@@ -1,22 +1,7 @@
 from tkinter import *
 from Modules.Math import *
 
-def read_object(path):
-    object = open(path, 'r')
-    ObjectArray = object.read().splitlines()
 
-    for index in range(len(ObjectArray)):
-        ObjectArray[index] = ObjectArray[index].split()
-    print(ObjectArray)
-    ObjectClassArray = []
-    for index in range(len(ObjectArray)):
-        if ObjectArray[index][0] == 'Resi' :
-            ObjectClassArray.append(Resistor(ObjectArray[index][1],ObjectArray[index][2],ObjectArray[index][3]))
-
-        if ObjectArray[index][0] == 'Link' :
-            ObjectClassArray.append(Link(ObjectArray[index][1],ObjectArray[index][2],ObjectArray[index][3],ObjectArray[index][4]))
-
-    print(ObjectClassArray[3].show_position())
-
-
-read_object('Maps/Map1.txt')
+print(read_object('Maps/Map1.txt'))
+#print(Generate_resistor(100,100))
+Generate_file('Maps/MapGen.txt')
