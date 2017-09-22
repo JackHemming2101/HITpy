c6accac172abd59e6dd6f4fff13c1c71389b59df
GlavPoci <glavpoci@gmail.com>
2017-09-22 11:35:33 +0300

    Version Math 0.1 Соеденения. Правки параметров

--- Modules/Math.py
+++ Modules/Math.py
@@ -1,8 +1,18 @@
 class Resistor:
-    def __init__(self,x,y):
+    def __init__(self,x,y,resist):
         self.position_x = x
         self.position_y = y
-
+        self.resist = resist
 
     def show_position(self):
         return([self.position_x,self.position_y,])
+
+class Link:
+    def __init__(self,x,y,x2,y2):
+        self.position_start_x = x
+        self.position_start_y = y
+        self.position_end_x = x2
+        self.position_end_y = y2
+
+    def show_position(self):
+        print(self.position_start_x,self.position_start_y,self.position_end_x,self.position_end_y)
--- index.py
+++ index.py
@@ -11,10 +11,12 @@ def read_object(path):
     ObjectClassArray = []
     for index in range(len(ObjectArray)):
         if ObjectArray[index][0] == 'Resi' :
-            ObjectClassArray.append(Resistor(ObjectArray[index][1],ObjectArray[index][2]))
+            ObjectClassArray.append(Resistor(ObjectArray[index][1],ObjectArray[index][2],ObjectArray[index][3]))
 
+        if ObjectArray[index][0] == 'Link' :
+            ObjectClassArray.append(Link(ObjectArray[index][1],ObjectArray[index][2],ObjectArray[index][3],ObjectArray[index][4]))
 
-    print(ObjectClassArray[1].show_position())
+    print(ObjectClassArray[3].show_position())
 
 
 read_object('Maps/Map1.txt')
