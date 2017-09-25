5c43e3452c98ba40e4e3d1b4622e6de5ba58f4a4
GlavPoci <glavpoci@gmail.com>
2017-09-25 17:10:09 +0300

    Тестовая вариаия прощётов

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from Modules.Math import *
 
 
 Objects = read_object('Maps/Map1.txt')
-print(Objects[0].check_connect(2))
+
 #print(Generate_resistor(100,100))
 Generate_file('Maps/MapGen.txt')
+print(find_res_map(Objects))
